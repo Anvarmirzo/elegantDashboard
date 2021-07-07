d007b661c234019bdd4b47a7fdf8653cc997d833
Anvar <www.latvienhoasadrizeges@gmail.com>
2021-07-07 19:55:53 +0500

    fixed bug with flicker dark theme

--- src/js/_dropzone-config.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  Dropzone.autoDiscover = false;
-  const dropzoneID = document.getElementById('dropzone');
-  if (dropzoneID) {
-    var myDropzone = new Dropzone(dropzoneID, { url: '/file/post' });
-  }
-})();
